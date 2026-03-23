034e00292f74c36df34e9525380a8eb6d0eab0e9
Shubham Vishwakarma <129714338+its-shubhu123@users.noreply.github.com>
2026-03-23 16:52:06 +0530

    Delete server directory

--- server/Routes/messageRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createMessage, getMessages } = require("../Controller/messageController");
-
-const router = express.Router();
-
-router.post("/",createMessage);
-router.get("/:chatId",getMessages);
-
-module.exports = router;
